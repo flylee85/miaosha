154199856d9ac2d56937fe40f8a8a43f7ec7aa9e
loserbird <15626140150@163.com>
2018-02-01 18:44:14 +0800

    秒杀接口地址隐藏，添加验证码

---
+++ src/main/java/com/loserbird/miaosha/access/UserContext.java
@@ -0,0 +1,18 @@
+package com.loserbird.miaosha.access;
+
+
+import com.loserbird.miaosha.domain.MiaoshaUser;
+
+public class UserContext {
+	
+	private static ThreadLocal<MiaoshaUser> userHolder = new ThreadLocal<MiaoshaUser>();
+	
+	public static void setUser(MiaoshaUser user) {
+		userHolder.set(user);
+	}
+	
+	public static MiaoshaUser getUser() {
+		return userHolder.get();
+	}
+
+}
--- src/main/java/com/loserbird/miaosha/config/UserArgumentResolver.java
+++ src/main/java/com/loserbird/miaosha/config/UserArgumentResolver.java
@@ -1,8 +1,8 @@
 package com.loserbird.miaosha.config;
 
+import com.loserbird.miaosha.access.UserContext;
 import com.loserbird.miaosha.domain.MiaoshaUser;
 import com.loserbird.miaosha.service.MiaoshaUserService;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Service;
@@ -11,44 +11,20 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 
 @Service
 public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 
-	@Autowired
+    @Autowired
     MiaoshaUserService userService;
-	
-	public boolean supportsParameter(MethodParameter parameter) {
-		Class<?> clazz = parameter.getParameterType();
-		return clazz==MiaoshaUser.class;
-	}
 
-	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-		HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-		HttpServletResponse response = webRequest.getNativeResponse(HttpServletResponse.class);
-		
-		String paramToken = request.getParameter(MiaoshaUserService.COOKI_NAME_TOKEN);
-		String cookieToken = getCookieValue(request, MiaoshaUserService.COOKI_NAME_TOKEN);
-		if(StringUtils.isEmpty(cookieToken) && StringUtils.isEmpty(paramToken)) {
-			return null;
-		}
-		String token = StringUtils.isEmpty(paramToken)?cookieToken:paramToken;
-		return userService.getByToken(response, token);
-	}
+    public boolean supportsParameter(MethodParameter parameter) {
+        Class<?> clazz = parameter.getParameterType();
+        return clazz==MiaoshaUser.class;
+    }
 
-	private String getCookieValue(HttpServletRequest request, String cookiName) {
-		Cookie[]  cookies = request.getCookies();
-		for(Cookie cookie : cookies) {
-			if(cookie.getName().equals(cookiName)) {
-				return cookie.getValue();
-			}
-		}
-		return null;
-	}
-
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return UserContext.getUser();
+    }
 }
